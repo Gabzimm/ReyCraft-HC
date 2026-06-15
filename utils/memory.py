2cb1c23463b8bd84928c1c6404182c5660b9f600
Gabzimm <gabriele.s.leal.pro@gmail.com>
2026-06-15 13:49:49 +0100

    Add memory management for guild data
    Implement functions to load and save guild-specific data in a JSON file.

---
+++ utils/memory.py
@@ -0,0 +1,57 @@
+import json
+import os
+from datetime import datetime
+
+# ========== ARQUIVO ÚNICO PARA TODAS AS CONFIGURAÇÕES ==========
+CONFIG_FILE = "bot_memory.json"
+
+def load_all_data():
+    """Carrega TODOS os dados salvos automaticamente"""
+    if os.path.exists(CONFIG_FILE):
+        try:
+            with open(CONFIG_FILE, "r") as f:
+                return json.load(f)
+        except:
+            return {}
+    return {}
+
+def save_all_data(data):
+    """Salva TODOS os dados automaticamente"""
+    try:
+        with open(CONFIG_FILE, "w") as f:
+            json.dump(data, f, indent=4)
+        return True
+    except:
+        return False
+
+def save_guild_data(guild_id, key, value):
+    """Salva dados específicos de um servidor"""
+    data = load_all_data()
+    
+    if str(guild_id) not in data:
+        data[str(guild_id)] = {}
+    
+    data[str(guild_id)][key] = value
+    data[str(guild_id)]['last_update'] = datetime.now().isoformat()
+    
+    save_all_data(data)
+
+def load_guild_data(guild_id, key, default=None):
+    """Carrega dados específicos de um servidor"""
+    data = load_all_data()
+    
+    if str(guild_id) in data:
+        return data[str(guild_id)].get(key, default)
+    
+    return default
+
+def load_all_guild_data(guild_id):
+    """Carrega TODOS os dados de um servidor"""
+    data = load_all_data()
+    return data.get(str(guild_id), {})
+
+def save_all_guild_data(guild_id, guild_data):
+    """Salva TODOS os dados de um servidor"""
+    data = load_all_data()
+    data[str(guild_id)] = guild_data
+    save_all_data(data)
